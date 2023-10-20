7970d369109c721d22c84e16f27d57e76d49339c
João Matheus Albergaria Rodrigues Santana <51250239+joaomatheusars@users.noreply.github.com>
2023-10-20 12:51:59 -0300

    chore: add s3 domain to next config file

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@prisma/client";
+import Image from "next/image";
 
 interface ProductItemProps {
     product: Product
@@ -6,7 +7,27 @@ interface ProductItemProps {
 
 const ProductItem = ({product}: ProductItemProps) => {
     return (
-        <h1>{product.name}</h1>
+        <div className="flex flex-col gap-4 max-w-[156px]">
+            <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+                <Image
+                    src={product.imageUrls[0]}
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="h-auto max-h-[70%] w-auto max-w-[80%]"
+                    style={
+                        {
+                            objectFit: "contain",
+                        }
+                    }
+                    alt={product.name}
+                />
+            </div>
+
+            <div>
+                <p className=" w-full text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
+            </div>
+        </div>
     );
 }
  
