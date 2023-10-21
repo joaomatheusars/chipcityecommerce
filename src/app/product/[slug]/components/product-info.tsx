f4c0c8e942c472f0dcf4dda8199c1a35f5a04875
João Matheus Albergaria Rodrigues Santana <51250239+joaomatheusars@users.noreply.github.com>
2023-10-21 12:35:24 -0300

    feat: ensure product can add to cart

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -2,25 +2,28 @@
 import { Button } from "@/components/ui/button";
 import DiscountBadge from "@/components/ui/discount-badge";
 import { ProductWithTotalPrice } from "@/helpers/product";
+import { CartContext } from "@/providers/cart";
 import {
   ArrowLeftIcon,
-  ArrowRight,
   ArrowRightIcon,
   TruckIcon,
 } from "lucide-react";
-import { useState } from "react";
+import { use, useContext, useState } from "react";
 
 interface ProductInfoProps {
-  product: Pick<
-    ProductWithTotalPrice,
-    "basePrice" | "description" | "discountPercentage" | "totalPrice" | "name"
-  >;
+  product: ProductWithTotalPrice
 }
 
 const ProductInfo = ({
-  product: { basePrice, description, discountPercentage, totalPrice, name },
+  product
 }: ProductInfoProps) => {
   const [quantity, setQuantity] = useState(1);
+
+  const {addProductToCart} = useContext(CartContext)
+
+  const handleAddToCartClick = () => {
+    addProductToCart({...product, quantity})
+  }
 
   const handleDecreaseQuantityClick = () => {
     setQuantity((prev) => (prev === 1 ? prev : prev - 1));
@@ -32,20 +35,20 @@ const ProductInfo = ({
 
   return (
     <div className="flex flex-col px-5">
-      <h2 className="text-lg">{name}</h2>
+      <h2 className="text-lg">{product.name}</h2>
 
       <div className="item-cente flex gap-2">
-        <h1 className="text-xl font-bold">R$ {totalPrice.toFixed(2)}</h1>
-        {discountPercentage > 0 && (
+        <h1 className="text-xl font-bold">R$ {product.totalPrice.toFixed(2)}</h1>
+        {product.discountPercentage > 0 && (
           <DiscountBadge className=" left-3 top-3 px-2 py-[2px]">
-            {discountPercentage}
+            {product.discountPercentage}
           </DiscountBadge>
         )}
       </div>
 
-      {discountPercentage > 0 && (
+      {product.discountPercentage > 0 && (
         <p className="text-xs opacity-50">
-          De R$ {Number(basePrice).toFixed(2)}
+          De R$ {Number(product.basePrice).toFixed(2)}
         </p>
       )}
 
@@ -71,10 +74,10 @@ const ProductInfo = ({
 
       <div className="mt-8 flex flex-col gap-3">
         <h3 className="text-base font-bold">Descrição</h3>
-        <p className="text-justify text-sm opacity-60">{description}</p>
+        <p className="text-justify text-sm opacity-60">{product.description}</p>
       </div>
 
-      <Button className="mt-8 font-bold uppercase">
+      <Button className="mt-8 font-bold uppercase" onClick={handleAddToCartClick}>
         Adicionar ao carrinho
       </Button>
 
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import { Product } from "@prisma/client";
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useState } from "react";
 
 interface CartProduct extends Product {
   quantity: number;
@@ -12,20 +12,29 @@ interface ICartContext {
   cartTotal: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
+  addProductToCart: (product: CartProduct) => void;
 }
 
-const CartContext = createContext<ICartContext>({
+export const CartContext = createContext<ICartContext>({
   products: [],
   cartBasePrice: 0,
   cartTotal: 0,
   cartTotalDiscount: 0,
+  addProductToCart: () => {},
 });
 
-const CartProvider = ({children}: {children: ReactNode}) => {
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const addProductToCart = (product: CartProduct) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
   return (
     <CartContext.Provider
       value={{
-        products: [],
+        products,
+        addProductToCart,
         cartBasePrice: 0,
         cartTotal: 0,
         cartTotalDiscount: 0,
