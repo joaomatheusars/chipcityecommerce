eb5111764c209033021ab2cef3af0257e073dbca
João Matheus <joaomatheusars@gmail.com>
2024-04-18 16:05:02 -0300

    add key in div

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -8,14 +8,14 @@ interface ProductListProps {
 
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex w-full gap-4 px-5 overflow-x-auto md:px-0 md:mt-4 md:overflow-hidden">
-      <div className="flex gap-5 md:grid md:grid-cols-7 md:gap-8 md:items-center md:justify-center">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 md:mt-4 md:overflow-hidden md:px-0">
+      <div className="flex gap-5 md:grid md:grid-cols-7 md:items-center md:justify-center md:gap-8">
         {products.map((product) => (
           <div className="flex" key={product.id}>
-            <ProductItem
-            key={product.id}
+            {<ProductItem
+              key={product.id}
               product={computeProducTotalPrice(product)}
-            />
+            />}
           </div>
         ))}
       </div>
