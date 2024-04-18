66c4582073d4bdecb18125ddee5003f2ca0a8659
João Matheus <joaomatheusars@gmail.com>
2024-04-18 15:54:18 -0300

    remove key

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -7,13 +7,14 @@ interface ProductListProps {
 }
 
 const ProductList = ({ products }: ProductListProps) => {
+  let count = 0;
   return (
     <div className="flex w-full gap-4 px-5 overflow-x-auto md:px-0 md:mt-4 md:overflow-hidden">
       <div className="flex gap-5 md:grid md:grid-cols-7 md:gap-8 md:items-center md:justify-center">
         {products.map((product) => (
           <div className="flex">
             <ProductItem
-              key={product?.id}
+              key={count++}
               product={computeProducTotalPrice(product)}
             />
           </div>
