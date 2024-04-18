7a0b826aaa1493654b718d571ff55ac165b3dded
João Matheus <joaomatheusars@gmail.com>
2024-04-18 16:10:45 -0300

    upgrade

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -11,9 +11,9 @@ const ProductList = ({ products }: ProductListProps) => {
     <div className="flex w-full gap-4 overflow-x-auto px-5 md:mt-4 md:overflow-hidden md:px-0">
       <div className="flex gap-5 md:grid md:grid-cols-7 md:items-center md:justify-center md:gap-8">
         {products.map((product) => (
-          <div className="flex" key={product.id}>
+          <div className="flex" key={product.id.toString()}>
             {<ProductItem
-              key={product.id}
+              key={product.id.toString()}
               product={computeProducTotalPrice(product)}
             />}
           </div>
