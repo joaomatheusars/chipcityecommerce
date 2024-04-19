8906d511e1655ece0e97e4390551aadbb3ebc1d9
João Matheus <joaomatheusars@gmail.com>
2024-04-19 14:40:04 -0300

    add key

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -1,6 +1,7 @@
 import ProductItem from "@/components/ui/product-item";
 import { computeProducTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
+import React from "react";
 
 interface ProductListProps {
   products: Product[];
@@ -11,12 +12,14 @@ const ProductList = ({ products }: ProductListProps) => {
     <div className="flex w-full gap-4 overflow-x-auto px-5 md:mt-4 md:overflow-hidden md:px-0">
       <div className="flex gap-5 md:grid md:grid-cols-7 md:items-center md:justify-center md:gap-8">
         {products.map((product) => (
-          <div className="flex" key={product.id.toString()}>
+          <React.Fragment key={product.id}>
+          <div className="flex" key={product.id}>
             {<ProductItem
-              key={product.id.toString()}
+              key={product.id}
               product={computeProducTotalPrice(product)}
             />}
           </div>
+          </React.Fragment>
         ))}
       </div>
     </div>
