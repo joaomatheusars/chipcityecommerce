10299277f19c747a7b27d60cbc87fee78df6f857
João Matheus <joaomatheusars@gmail.com>
2024-04-19 14:50:12 -0300

    add key

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
     },
     experimental: {
         serverActions:true,
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
